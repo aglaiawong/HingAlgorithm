858b5156d10141db23be45c73bb44d6ad051d8ae
Hing Aglaia Wong <aglaiawong@users.noreply.github.com>
2018-07-23 23:02:16 +0800

    690

---
+++ 690_Employee_Importance.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int getImportance(vector<Employee*> employees, int id) {
+                     //id,index pair
+        unordered_map<int,int > um;
+        int n=employees.size();
+        for(int i=0;i<n;i++)
+            um[employees[i]->id]=i;
+        queue<int> Q;
+        Q.push(id);
+        int ans=0;
+        while(Q.empty()!=true){
+            id=Q.front();
+            Q.pop();
+            ans += employees[um[id]]->importance;
+            int n=employees[um[id]]->subordinates.size();
+            
+            for(int i=0;i<n;i++)
+                Q.push(employees[um[id]]->subordinates[i]);
+        }
+        return ans;
+    }
+};
