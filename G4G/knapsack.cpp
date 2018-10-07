03537b7923101837d1935d31989b71623e0b7f4e
Hing Aglaia Wong <aglaiawong@users.noreply.github.com>
2018-10-08 07:45:17 +0800

    DP: knapsack problem

--- G4G/knapsack.cpp
+++ G4G/knapsack.cpp
@@ -14,10 +14,15 @@ int knapsack(int W, int wt[], int val[], int n){
 	int i,w;	// 'w': current knapsack capacity; 'n': current item to csdr
 	int K[n+1][W+1];	//why '+1'? basis for packing items in a 0 capacity knapsack. 
 	
+	
+	/*
+		prove of correctness of nested loop:
+		for evey item, consider all weights to contain it. 
+	*/
 	for(i=0;i<n;i++){
 		for(w=0; w<=W; w++){
 			if(i==0 || w == 0) K[i][w]=0;
-			else if(wt[i-1] <= w)	
+			else if(wt[i-1] <= w)	//when still hv space there are 2 choices: whether to pack/not pack a particular item i; 
 				K[i][w] = max(val[i-1]+K[i-1][w-wt[i-1]], K[i-1][w]);
 				//all got '-1' cz original K[n+1][W+1] covers special case
 				// where item = 0 and capacity = 0; 
