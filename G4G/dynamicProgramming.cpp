2aab5e992ff185734f172835e927469cc76d318a
Hing Aglaia Wong <aglaiawong@users.noreply.github.com>
2018-10-07 11:03:38 +0800

    dp

---
+++ G4G/dynamicProgramming.cpp
@@ -0,0 +1,28 @@
+/*
+DP by tabulation
+to find factorial x
+*/
+
+int factorial(int x){
+	int dp[MAXN];
+	int dp[0] = 1;	
+	
+	for(int i=1; i<=n; i++){
+		dp[i] = dp[i-1]*i;		//rmb, here always starts/builds from base case
+	}
+}
+
+// Memoized version to find factorial x
+
+int dp[MAXN];		//init to -1
+int factorial(int x){
+	if(x==0)
+		return 1;
+	if(dp[x]!=-1)
+		return dp[x];
+	return (dp[x] = x * factorial(dp[i-1]));
+	// peel onion layer-by-layer; 
+	// returning the value == layer-by-layer, rebuild the onion tgt 
+}
+
+
