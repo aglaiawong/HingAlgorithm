2df8ddf74466b073c0a2e7afe5f884d61945a697
Hing Aglaia Wong <aglaiawong@users.noreply.github.com>
2018-10-15 07:54:06 +0800

    dp_uglyNumber

---
+++ G4G/dp_uglyNumber.cpp
@@ -0,0 +1,8 @@
+/* Ugly number@G4G: https://www.geeksforgeeks.org/ugly-numbers/
+Find the nth Ugly number.
+- i.e. define ugly number: Ugly numbers are numbers whose only prime factors are 2, 3 or 5.
+- i.e. ensure no other prime factor. <-- how to achieve this? 
+- check the result of division, if even, let it go;
+  otherwise, i.e. if odd, recursively divide into base case and see if the result belongs to {2,3,5}
+*/
+
